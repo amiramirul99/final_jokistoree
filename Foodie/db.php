ad7c09dec400a4e08d2b8b4e70c5b25adb05d084
amiramirul99 <64324729+amiramirul99@users.noreply.github.com>
2020-06-25 16:33:07 +0800

    joki store

---
+++ Foodie/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "foodie";
+
+// Create connection
+$con = mysqli_connect($host, $username, $password, $db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
